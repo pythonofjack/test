b34154d178753148dc6872aeabdad1fc80f1475d
jk <javaofjkbiz@gmail.com>
2023-08-10 15:55:42 +0900

    20230810

--- test.py
+++ test.py
@@ -5,10 +5,14 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 options = Options()
 options.add_experimental_option("detach",True)#브라우저 바로닫힘 방지
-
-# chrome_driver = ChromeDriverManager().install() #버전3용
+options.add_experimental_option("excludeSwitches",["emable-logging"])#알림방지
+options.add_argument("--start-maximized")#화면최대크기
+options.add_argument("--disable-blink-features=AutomationControlled")#자동화아님으로 눈속임
+# driver = ChromeDriverManager().install() #버전3용
 service = Service(ChromeDriverManager().install()) #버전4용
 
 driver = webdriver.Chrome(service=service,options=options)
 
 driver.get("http://naver.com")
+
+# driver.quit()#화면닫기
